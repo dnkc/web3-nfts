14ff121fb336838b369e0588e75e3d0723593285
dnkc <dnkcdesign@gmail.com>
2022-04-27 20:25:56 -0500

    first commit - nft/id finished

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,75 @@
+import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react'
+
+function NFTDropPage() {
+  const connectWithMetamask = useMetamask()
+  const address = useAddress()
+  const disconnect = useDisconnect()
+  //   console.log(address)
+
+  return (
+    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+      {/* Left */}
+      <div className="bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4">
+        <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
+          <div className="rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600">
+            <img
+              className="w-44 rounded-xl object-cover p-2 lg:h-96 lg:w-72"
+              src="https://links.papareact.com/8sg"
+              alt="apes"
+            />
+          </div>
+          <div className="space-y-2 p-5 text-center">
+            <h1 className="text-4xl font-bold text-white">REACT Apes</h1>
+            <h2 className="text-xl text-gray-300">
+              A collection of apes who live and breathe React!
+            </h2>
+          </div>
+        </div>
+      </div>
+      {/* Right */}
+      <div className="flex flex-1 flex-col p-12 lg:col-span-6">
+        {/* Header */}
+        <header className="items-cener flex justify-between">
+          <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+            The{' '}
+            <span className="font-extrabold underline decoration-pink-600/50">
+              REACT
+            </span>{' '}
+            NFT Market Place
+          </h1>
+          <button
+            onClick={() => (address ? disconnect() : connectWithMetamask())}
+            className="rounded-full bg-gradient-to-br from-cyan-800 to-rose-600 px-4 py-2 text-xs font-bold text-white duration-200 hover:shadow-md lg:px-5 lg:py-3 lg:text-base"
+          >
+            {address ? 'Sign Out' : 'Sign In'}
+          </button>
+        </header>
+        <hr className="my-2 border border-yellow-300" />
+        {address && (
+          <p className="text-center text-sm text-rose-400">
+            You're logged in with wallet {address.substring(0, 5)}...
+            {address.substring(address.length - 5)}
+          </p>
+        )}
+        {/* Content */}
+        <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
+          <img
+            className="w-80 object-cover pb-10 lg:h-40"
+            src="https://links.papareact.com/bdy"
+            alt=""
+          />
+          <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
+            The REACT Ape NFT Club | NFT Drop
+          </h1>
+          <p className="pt-2 text-xl text-green-500">13 / 21 NFT's claimed</p>
+        </div>
+        {/* Mint Button */}
+        <button className="mt-10 h-16 w-full rounded-full bg-red-500 font-bold text-white">
+          Mint NFT (0.01 ETH)
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default NFTDropPage
